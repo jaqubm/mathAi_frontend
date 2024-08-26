1e14ffd6a7d2036dcbaf89e75803825793102abc
jaqubm <jaqobgm@gmail.com>
2024-08-26 21:31:41 +0200

    Implemented shadcn/ui library and Aceternity UI with framer-motion and configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,26 @@
-import Image from "next/image";
+import React from "react";
+import {Vortex} from "@/components/ui/vortex";
+import { Button } from "@/components/ui/button"
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+export default function VortexDemo() {
+    return (
+        <div className="w-full mx-auto rounded-md  h-screen overflow-hidden">
+            <Vortex
+                backgroundColor="black"
+                className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+            >
+                <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
+                    mathAi
+                </h2>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
+                    Work in progress!
+                </p>
 
-    </main>
-  );
+                <Button variant='secondary' className='mt-6'>
+                    Click me!
+                </Button>
+            </Vortex>
+        </div>
+    );
 }
