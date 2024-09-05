d72b3e14cf0985baca463f96faeb19ef65d55bb3
jaqubm <jaqobgm@gmail.com>
2024-09-05 19:23:01 +0200

    Implemented simple navbar + theme with mode-toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "@/styles/globals.css";
 import React from "react";
+import {ThemeProvider} from "@/components/theme-provider";
+import {Navbar} from "@/components/navbar/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar/>
+
+          {children}
+
+        </ThemeProvider>
+
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React from "react";
-import {Vortex} from "@/components/ui/vortex";
 import { Button } from "@/components/ui/button"
 import {useRouter} from "next/navigation";
 
@@ -13,23 +12,18 @@ export default function VortexDemo() {
     }
 
     return (
-        <div className="w-full mx-auto rounded-md  h-screen overflow-hidden">
-            <Vortex
-                backgroundColor="black"
-                className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
-            >
-                <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-                    mathAi
-                </h2>
+        <div className="w-full mx-auto rounded-md  h-screen overflow-hidden flex flex-col justify-center items-center">
+            <h2 className="md:text-6xl font-bold text-center">
+                mathAi
+            </h2>
 
-                <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-                    Work in progress!
-                </p>
+            <p className="text-sm md:text-2xl max-w-xl mt-6 text-center">
+                Work in progress!
+            </p>
 
-                <Button variant='secondary' className='mt-6' onClick={redirectApiStatus}>
-                    API Status
-                </Button>
-            </Vortex>
+            <Button variant='secondary' className='mt-6 w-fit' onClick={redirectApiStatus}>
+                Status
+            </Button>
         </div>
     );
 }
