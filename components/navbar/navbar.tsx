40bcc19d2e4ba5555b9e7a48ea8578bbf5ce26d1
jaqubm <jaqobgm@gmail.com>
2024-10-08 18:09:03 +0200

    Bugfix for FirstTimeSignIn Dialog window

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -23,7 +23,7 @@ import {
 import {ClientNavbar} from "@/components/navbar/client-navbar";
 import {DialogTrigger} from "@/components/ui/dialog";
 import {UserExerciseSetsDialog} from "@/components/navbar/user-exercise-sets-dialog";
-import {signIn, signOut, useSession} from "next-auth/react";
+import {signOut, useSession} from "next-auth/react";
 import {FirstTimeSignIn, IsTeacher, UpdateToStudent, UpdateToTeacher} from "@/app/api/user";
 import {useRouter} from "next/navigation";
 import {handleServerSignIn, handleServerSignOut} from "@/app/api/auth";
