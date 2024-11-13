a8efd5038b3611f2d417c405e4adaff0b84edfcf
jaqubm <jaqobgm@gmail.com>
2024-11-13 20:59:45 +0100

    Work in progress class page

--- app/class/[id]/page.tsx
+++ app/class/[id]/page.tsx
@@ -5,7 +5,10 @@ import React, {useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
 import {useSession} from "next-auth/react";
 import {getClass} from "@/app/api/class";
-import {Class, User} from "@/app/api/types";
+import {AssignmentList, Class, User} from "@/app/api/types";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {ScrollArea} from "@/components/ui/scroll-area";
+import {Separator} from "@/components/ui/separator";
 
 export default function ClassPage({ params }: { params: { id: string } }) {
     const router = useRouter()
@@ -36,8 +39,6 @@ export default function ClassPage({ params }: { params: { id: string } }) {
             })
     }, [params.id, session?.user?.email])
 
-    console.log(cClass)
-
     return (
         <>
             {loading && (
@@ -54,12 +55,80 @@ export default function ClassPage({ params }: { params: { id: string } }) {
                     </div>
 
                     {/* Displaying exercises from exerciseSet */}
-                    <div className="grid gap-5">
-                        {cClass.students.map((student: User, index: number) => (
-                            <div key={student.email}>
-                                {student.name}
-                            </div>
-                        ))}
+                    <div className="w-full flex md:flex-row flex-col justify-center items-center gap-2">
+
+                        <Card className="flex-1 w-full">
+                            <CardHeader>
+                                <CardTitle>Studenci</CardTitle>
+                            </CardHeader>
+
+                            <CardContent>
+
+                                {cClass.students.length === 0 && (
+                                    <div>
+                                        Lista studnetów jest pusta!
+                                    </div>
+                                )}
+
+                                {cClass.students.length > 0 && (
+                                    <ScrollArea className="max-h-80 overflow-y-scroll">
+                                        <ul>
+                                            {cClass.students.map((student: User, index: number) => (
+                                                <div key={student.email}>
+                                                    <div>
+                                                        <li className="font-bold">{student.name}</li>
+                                                        <p className="text-sm">
+                                                            {student.email}
+                                                        </p>
+                                                    </div>
+                                                    {index !== cClass.students.length - 1 && <Separator className="my-2" />}
+                                                </div>
+                                            ))}
+                                        </ul>
+                                    </ScrollArea>
+                                )}
+
+                            </CardContent>
+
+                            <CardFooter>
+                                To be added..
+                            </CardFooter>
+                        </Card>
+
+                        <Card className="flex-1 w-full">
+                            <CardHeader>
+                                <CardTitle>Zadania</CardTitle>
+                            </CardHeader>
+
+                            <CardContent>
+
+                                {cClass.assignments.length === 0 && (
+                                    <div>
+                                        Lista zadań jest pusta!
+                                    </div>
+                                )}
+
+                                {cClass.assignments.length > 0 && (
+                                    <ScrollArea className="max-h-80 overflow-y-scroll">
+                                        <ul>
+                                            {cClass.assignments.map((assignment: AssignmentList, index: number) => (
+                                                <div key={assignment.classId + assignment.exerciseSetId}>
+                                                    <div>
+                                                        <li>{assignment.name}</li>
+                                                    </div>
+                                                    {index !== cClass.assignments.length - 1 && <Separator className="my-2" />}
+                                                </div>
+                                            ))}
+                                        </ul>
+                                    </ScrollArea>
+                                )}
+
+                            </CardContent>
+
+                            <CardFooter>
+                                To be added..
+                            </CardFooter>
+                        </Card>
                     </div>
                 </div>
             )}
